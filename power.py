210cd96c26f5fbd027d3b2797323c92dd8aca256
AlessMor <alessandro.morandi@polito.it>
2025-05-11 10:50:47 +0200

    calculation of P_e_net now can be done either by using Q or P_aux
    At the moment Paux is set in order to evaluate P_e_net

--- power.py
+++ power.py
@@ -1,6 +1,6 @@
 from sigmav_functions import sigmav_DT_BoschHale
 
-def calculate_P_e_net(Pf:float, Q:float, eta_th:float) -> float:
+def calculate_P_e_net_Q(Pf:float, Q:float, eta_th:float) -> float:
     r"""Calculate the net electrical power produced by the reactor.
 
     Args:
@@ -14,7 +14,24 @@ def calculate_P_e_net(Pf:float, Q:float, eta_th:float) -> float:
     # Q = (Pf - Paux)/Paux then Paux = Pf/(Q+1)
     P_aux = Pf / (Q + 1) # [W] is the auxiliary heating power needed to maintain the plasma temperature
     P_e_net = eta_th * Pf - P_aux # [W] is the net electrical power produced by the reactor
-    return P_e_net
+    return P_e_net, P_aux
+
+def calculate_P_e_net_Paux(Pf:float, P_aux:float, eta_th:float) -> float:
+    r"""Calculate the net electrical power produced by the reactor.
+
+    Args:
+        Pf: Fusion power [W]
+        Q: Auxiliary heating power [W]
+        eta_th: Thermal efficiency of the reactor [-]
+
+    Returns:
+        P_e_net: Net electrical power produced by the reactor [W]
+    """
+    Q = (Pf - P_aux)/P_aux 
+    # P_aux = Pf / (Q + 1) # [W] is the auxiliary heating power needed to maintain the plasma temperature
+    P_e_net = eta_th * Pf - P_aux # [W] is the net electrical power produced by the reactor
+    return P_e_net, Q
+
 
 def fusion_power_50D50T(n_e_avg, T_e_avg, E_DT, V_plasma):
     """
